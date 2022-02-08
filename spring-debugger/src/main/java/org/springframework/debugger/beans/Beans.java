acfbbf256202282d90171dd2c8049f855c582b0d
lizza <liutao_glory@126.com>
2022-02-08 13:32:11 +0800

    commit

---
+++ spring-debugger/src/main/java/org/springframework/debugger/beans/Beans.java
@@ -0,0 +1,18 @@
+package org.springframework.debugger.beans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Desc:
+ * @author: lizza.liu
+ * @date: 2022-02-08
+ */
+public class Beans {
+
+	public static void main(String[] args){
+		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:beans/beans.xml");
+		User user = context.getBean("user", User.class);
+		System.out.println(user);
+	}
+}
---
+++ spring-debugger/src/main/java/org/springframework/debugger/beans/User.java
@@ -0,0 +1,37 @@
+package org.springframework.debugger.beans;
+
+/**
+ * @Desc:
+ * @author: lizza.liu
+ * @date: 2022-02-08
+ */
+public class User {
+
+	private String name;
+
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"name='" + name + '\'' +
+				", age=" + age +
+				'}';
+	}
+}
